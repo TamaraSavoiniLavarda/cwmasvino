71d5ad5821f09d6628090094ac0276ff52557c67
TamaraSavoiniLavarda <tamarasavoini997@gmail.com>
2020-11-17 04:12:32 -0300

    avance pagina 3 y arreglos de cursos y single

--- single.php
+++ single.php
@@ -8,20 +8,43 @@ if (have_posts()): the_post();
     $imagen = get_the_post_thumbnail_url();
     // $categoria = get_the_title();
     $contenido = get_the_content();
-    $link = get_the_permalink();
+	$link = get_the_permalink();
+
+	$auxCat = get_the_terms($postid, 'category');
+	$categoria = '';
+	foreach ($auxCat as $categorias) {
+		foreach ($categorias as $key => $value) {
+		if($key === 'name'){
+			$categoria .= $value . ', ';
+		}
+		}
+	}
+	$categoria = substr($categoria, 0, -2) . '.';
   ?>
-  <section class="container">
-  	<div class="row">
-	  <div class="col-lg-12">
-	    <div class="contenedor_cursos">
-	      <img src="<?=$imagen?>" alt="<?=$titulo?>">
-	      <h3 class="titulo2"><?=$titulo?></h3>
-	      <p class="clases_online">Clases online</p>
-	      <h3 class="precio"><?=$contenido?></h3>
-	    </div>
-	  </div>
-  	</div>
-  </section>
+	<section class="contenedor_curso">
+		<div class="container-fluid contenedor__mod_curso">
+			<div class="row">
+				<div class="col-lg-7">
+					<div>
+						<h3 class="titulo__curso"><?=$titulo?></h3>
+					</div>
+					<div>
+						<p class="clases_online_curso"><?=$categoria?></p>
+						<h3 class="precio_curso"><?=$contenido?></h3>
+					</div>
+					<div class="contenedor_añadir_mp">
+						<div>
+							<a href="<?=$link?>" class="añadir__carrito">Añadir al carrito</a>
+							<img src="<?= getIMG('mercadolibre.png')?>" alt="mercadolibre">
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-5">
+						<img src="<?=$imagen?>" alt="<?=$titulo?>">
+				</div>
+			</div>
+		</div>
+	</section>
 <?php else: ?>
 	<p>El post no esta disponible</p>
 <?php endif; ?>
