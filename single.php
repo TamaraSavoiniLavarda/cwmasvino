9cbc7f4b0e7e8ec2bb47b90d790db2c740f6a7e6
TamaraSavoiniLavarda <tamarasavoini997@gmail.com>
2020-11-17 20:35:45 -0300

    cambios pagina 3 zona de orientación

--- single.php
+++ single.php
@@ -21,6 +21,7 @@
 	}
 	$categoria = substr($categoria, 0, -2);
 ?>
+<!-- ENTRADA DE CURSO INDIVIDUAL -->
 <div class="entrada__curso">
 	<section class="contenedor__titulo">
 		<div class="container-fluid contenedor__mod_curso">
@@ -50,6 +51,12 @@
 			</div>
 		</div>
 	</div>
+</div>
+<!-- CONTENIDO -->
+<div class="orientacion">
+	<p>
+		El curso está orientado a personas que se inician por primera vez en el conocimiento del vino o que tienen una base y quieren especializarse en el tema.</br>Durante el cursado les haremos una introducción al mundo del vino, recorriendo sus principales pilares temáticos en el marco de una experiencia teórica/práctica, que les permitirá conocer los matices y aspectos característicos de cada cepa y estilos de vino.</br><strong>Las clases se dictarán en vivo</strong> a través de la plataforma virtual <strong>Zoom</strong>, muy fácil de usar y se puede utilizar tanto de una computadora como de un dispositivo móvil.</br>Son <strong>4 clases</strong> una vez por semana con una duración de 2 hs. cada una.
+	</p>
 </div>
 <?php else: ?>
 	<p>El post no esta disponible</p>
